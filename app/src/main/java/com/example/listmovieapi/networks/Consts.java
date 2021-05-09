ceb45bd937abbd0c2ee66475b6230395f965501e
Alika Oktaviani <54714600+alkokt@users.noreply.github.com>
2021-05-09 17:44:44 +0800

    Update Consts.java

--- app/src/main/java/com/example/listmovieapi/networks/Consts.java
+++ app/src/main/java/com/example/listmovieapi/networks/Consts.java
@@ -3,7 +3,7 @@ package com.example.listmovieapi.networks;
 public class Consts {
 
     public static final String BASEURL = "https://api.themoviedb.org/3/";
-    public static final String APIKEY = "4203b6396602483a6c77144d291de012";
+    public static final String APIKEY = "ec7c6b14753099ace354a3be71323280";
     public static final String LANGUAGE = "en-US";
     public static final String PAGE = "1";
     public static final String IMAGEBASEURL = "https://image.tmdb.org/t/p/w500/";
